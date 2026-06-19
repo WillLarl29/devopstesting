acb73ce0a2c1c42183b27d8b1354b64e01b41690
Willman Alca Alfaro <22101735@ue.edu.pe>
2026-06-18 21:22:59 -0500

    cambios4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DevOps Testing  <code>CI-CD</code> ESAN Project
+          DevOps Testing  <code>CI-CD</code> ESAN Project (0)
         </p>
 
           Learn more on React CI/CD Pipeline
